9eb4dc8cc95329a88c981ef130905bb7d9b6dfc7
cindypenela <cindypenela@gmail.com>
2023-08-17 17:41:57 -0300

    Div funciona
    Div funciona

---
+++ eventos.js
@@ -0,0 +1,9 @@
+const contenedor = document.getElementById('contenedor');
+
+contenedor.style.padding = '5px';
+contenedor.style.backgroundColor = 'green';
+
+
+contenedor.addEventListener('click', function() {
+    alert('Hola! Soy el div');
+});
