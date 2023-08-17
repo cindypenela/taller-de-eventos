7fe9616477b1f7ba2a4307a31380a46d9c104b47
cindypenela <cindypenela@gmail.com>
2023-08-17 19:23:24 -0300

    Solución
    Solución

--- eventos.js
+++ eventos.js
@@ -1,9 +1,17 @@
 const contenedor = document.getElementById('contenedor');
+const boton = document.querySelector('button');
+
 
 contenedor.style.padding = '5px';
 contenedor.style.backgroundColor = 'green';
 
 
-contenedor.addEventListener('click', function() {
+contenedor.addEventListener('click', function(event) {
     alert('Hola! Soy el div');
+    event.stopPropagation();
+});
+
+boton.addEventListener('click', function(event) {
+    alert('¡Hola!');
+    event.stopPropagation();
 });
